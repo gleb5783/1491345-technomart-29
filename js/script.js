2446af13680fc13f2da2329b183c8210668e248a
gleb <44523822+gleb5783@users.noreply.github.com>
2020-11-04 13:31:28 +0300

    добавил js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+let modal = document.querySelector(".feedback");
+let popup = document.querySelector(".modal-window");
+let close = document.querySelector(".close");
+let map = document.querySelector(".show-map");
+let bigMap = document.querySelector(".modal-map");
+let closeMap = bigMap.querySelector(".close");
+
+modal.addEventListener("click", function (evt){
+	evt.preventDefault();
+	popup.classList.remove("modal-hiden");
+});
+
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-hiden");
+});
+
+map.addEventListener("click", function (evt){
+	evt.preventDefault();
+	bigMap.classList.remove("modal-hiden");
+});
+
+closeMap.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	bigMap.classList.add("modal-hiden");
+});
