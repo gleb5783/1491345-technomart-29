230c9f238a303ca2ae1c0e02352ca481de5bfbff
gleb <44523822+gleb5783@users.noreply.github.com>
2020-11-05 20:47:34 +0300

    js анимация popup

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 let modal = document.querySelector(".feedback");
 let popup = document.querySelector(".modal-window");
 let close = document.querySelector(".close");
@@ -10,22 +5,25 @@ let map = document.querySelector(".show-map");
 let bigMap = document.querySelector(".modal-map");
 let closeMap = bigMap.querySelector(".close");
 
-modal.addEventListener("click", function (evt){
-	evt.preventDefault();
-	popup.classList.remove("modal-hiden");
+
+modal.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-hiden");
+    popup.classList.add("modal-show");
 });
 
-close.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popup.classList.add("modal-hiden");
+close.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-hiden");
+    popup.classList.remove("modal-show");
 });
 
-map.addEventListener("click", function (evt){
-	evt.preventDefault();
-	bigMap.classList.remove("modal-hiden");
+map.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    bigMap.classList.remove("modal-hiden");
 });
 
-closeMap.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	bigMap.classList.add("modal-hiden");
+closeMap.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    bigMap.classList.add("modal-hiden");
 });
